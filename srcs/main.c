6de17335c091211d85b670375cd0e4ea9a4d9c52
ziggy <ziggyvandeven@gmail.com>
2023-09-06 17:50:44 -0400

    C2 06/09

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,29 @@
 /*   By: zvan-de- <zvan-de-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 13:19:45 by zvan-de-          #+#    #+#             */
-/*   Updated: 2023/09/06 14:31:05 by zvan-de-         ###   ########.fr       */
+/*   Updated: 2023/09/06 17:49:34 by zvan-de-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include <readline/readline.h>
+#include <signal.h>
+
+void sigint_handler() 
+{
+    readline("Minishell: ");
+}
+
+void sigkill_handler() 
+{
+   return;
+}
+
+void set_signals()
+{
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, sigkill_handler);
+}
 
 int main()
 {
@@ -19,9 +36,11 @@ int main()
 	char *line;
 
 	printf("\x1b[31m🔥🔥🔥WELCOME TO MINIHELL🔥🔥🔥🔥\x1b[0m\n");
+	set_signals();
 	while (i--)
 	{
 		line = (readline("Minishell: "));
+		add_history(line);
 		free(line);
 		line = NULL;
 		// Test Github

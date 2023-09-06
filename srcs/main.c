3d8cafae0291eb76756b1dee9e7592a7e9fff762
Olivier Roy <olivierroy123@gmail.com>
2023-09-06 14:06:00 -0400

    Test

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zvan-de- <zvan-de-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oroy <oroy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 13:19:45 by zvan-de-          #+#    #+#             */
-/*   Updated: 2023/09/06 13:58:50 by zvan-de-         ###   ########.fr       */
+/*   Updated: 2023/09/06 14:04:28 by oroy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ int main()
 {
 	int i = 5;
 	char *line;
+	
 	printf("\x1b[31m🔥🔥🔥WELCOME TO MINIHELL🔥🔥🔥🔥\x1b[0m\n");
 	while (i--)
 	{
 		line = (readline("Minishell: "));
 		free(line);
 		line = NULL;
+		
 	}
 }
